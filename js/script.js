3f5d1ef9f7618b6a974fcd0c57dbef02b3162625
Soorya Kriz <sooryakriz111@gmail.com>
2021-03-02 18:33:19 +0530

    Nav

--- js/script.js
+++ js/script.js
@@ -2,26 +2,33 @@
 ===========================================*/
 
 // //Getting Menu
-// var menu = document.querySelector('.menu')
+var menu = document.querySelector('.menu')
 
 // //Getting Nav
-// var nav = document.querySelector('.nav')
+var nav = document.querySelector('.nav-container')
 
-//Getting Yellow Containers
-var yellowContainers = document.querySelectorAll('.yellow-container')
-
-// //Getting Stripes-Wrapper
-// var stripesWrapper = document.querySelector('.stripes-wrapper')
+//Nav Links
+var navLinks=document.querySelectorAll('.nav-items')
 
 // //menuNav Toggle Function
-// const menuNavToggle = () => {
-//     menu.classList.toggle('active')
-//     nav.classList.toggle('active')
-//     stripesWrapper.classList.toggle('active')
-// }
+const menuNavToggle = () => {
+    menu.classList.toggle('active')
+    nav.classList.toggle('active')
+}
+
+//Function navClose
+const navClose=()=>{
+//  Removing Nav Active Class
+nav.classList.remove('active')
+//  Removing Menu Active Class
+menu.classList.remove('active')
+}
 
 // //Adding Click Listener
-// menu.addEventListener('click',/* Menu & Nav Active Class Toggle Function*/  menuNavToggle );
+menu.addEventListener('click',/* Menu & Nav Active Class Toggle Function*/  menuNavToggle);
+
+//Nav Links Click Nav Close
+navLinks.forEach(element => /*Click Listener A Links*/element.addEventListener('click',navClose));
 
 // /* End 
 // ================================*/
@@ -34,8 +41,20 @@ var cursor = document.querySelector('.cursor')
 //Getting Buttons
 var buttons = document.querySelectorAll('button')
 
+//Getting Yellow Containers
+var yellowContainers = document.querySelectorAll('.yellow-container')
+
+//Getting anchor tags
+var aLinks = document.querySelectorAll('a')
+
 // Function triggerCursor
 const triggerCursor = (e) =>/*Styling Cursor */ cursor.setAttribute('style', `top:${e.pageY}px;left:${e.pageX}px`)
+
+//Function menuCursorAnimation
+const menuCursorAnimation = () => {cursor.classList.add('active'); menu.classList.add('menuActive');}
+
+//Function remMenuCursorAnimation
+const remMenuCursorAnimation = () => {cursor.classList.remove('active');menu.classList.remove('menuActive');}
 
 //Function btnCursorAnimation
 const btnCursorAnimation = () => cursor.classList.add('active')
@@ -43,27 +62,43 @@ const btnCursorAnimation = () => cursor.classList.add('active')
 //Function remBtnCursorAnimation
 const remBtnCursorAnimation = () => cursor.classList.remove('active')
 
+// Function aCursorAnimation
+const aCursorAnimation = () => cursor.classList.add('active')
+
+//Function remACursorAnimation
+const remACursorAnimation = () => cursor.classList.remove('active')
+
+//Function cursorOnYellow
+const cursorOnYellow = () => cursor.classList.add('cursor-yellow')
+
+//Function remCursorOnYellow
+const remCursorOnYellow = () => cursor.classList.remove('cursor-yellow')
+
 //Mouse Movie Listener To Cursor
 window.addEventListener('mousemove', triggerCursor)
 
+//Menu Animation Cursor
+menu.addEventListener('mouseover',menuCursorAnimation)
+menu.addEventListener('mouseleave',remMenuCursorAnimation)
 //Buttons Animation Cursor
 buttons.forEach(element => {
-    console.log(element);
     element.addEventListener('mouseover', btnCursorAnimation);
     element.addEventListener('mouseleave', remBtnCursorAnimation);
 })
 
-//Function cursorOnYellow
-const cursorOnYellow=()=>cursor.classList.add('cursor-yellow')
+//Anchot Tag Animation Cursor
+aLinks.forEach(element => {
+    element.addEventListener('mouseover', aCursorAnimation);
+    element.addEventListener('mouseleave', remACursorAnimation);
+})
 
-//Function remCursorOnYellow
-const remCursorOnYellow=()=>cursor.classList.remove('cursor-yellow')
 
 //Yellow Container MouseMove 
-yellowContainers.forEach((yContainer)=>{
-    yContainer.addEventListener('mouseenter',cursorOnYellow)
-    yContainer.addEventListener('mouseleave',remCursorOnYellow)
+yellowContainers.forEach((yContainer) => {
+    yContainer.addEventListener('mouseenter', cursorOnYellow)
+    yContainer.addEventListener('mouseleave', remCursorOnYellow)
 })
 
 /* End
 ================================================ */
+
